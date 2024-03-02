0390ac26d2677b511838fc3cce8b7106ef5fa4bc
anoosha-everest <160118595+anoosha-everest@users.noreply.github.com>
2024-03-02 11:32:37 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+const col=document.querySelector(".name");
+function red(){
+    console.log("hello");
+    col.innerHTML="Red";
+    col.style.color="red";
+}
+function green(){
+    console.log("hello");
+    col.innerHTML="Green";
+    col.style.color="green";
+}
+function blue(){
+    console.log("hello");
+    col.innerHTML="Blue";
+    col.style.color="blue";
+}
+function yellow(){
+    console.log("hello");
+    col.innerHTML="Yellow";
+    col.style.color="yellow";
+}
